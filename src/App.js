350d8786096e87af176d4ca212620f69b6df77e1
AbolnikovAnton <bakunin.abolnikov@gmail.com>
2022-01-05 21:54:40 +0200

    add redux thunk

--- src/App.js
+++ src/App.js
@@ -1,18 +1,16 @@
-import axios from 'axios';
 import React, { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import { Route, Routes } from "react-router-dom";
 import { Header } from './components';
 import { Cart, Home } from './pages';
-import { setPizzas } from './redux/actions/pizzas';
+import { fetchPizzas } from './redux/actions/pizzas';
 
 
 function App() {
   const dispatch = useDispatch()
 
   useEffect(() => {
-    axios.get('http://localhost:3001/pizzas')
-      .then(({ data }) => dispatch(setPizzas(data)))
+    dispatch(fetchPizzas())
   }, [])
 
   return (
